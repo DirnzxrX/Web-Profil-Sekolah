6575a3c0d456ee487cdfd0c8dac7b5b9205f2dde
DirnzxrX <andirodwitama@gmail.com>
2025-08-01 09:30:27 +0700

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-expand-lg navbar-modern">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">
+      <img src="assets/images/logo1.jpg" alt="Logo SMK">
+      SMK Negeri 64 Jakarta
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">
+            <i class="fas fa-home"></i> Beranda
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="profil.php">
+            <i class="fas fa-info-circle"></i> Profil
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="berita.php">
+            <i class="fas fa-newspaper"></i> Berita
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="kontak.php">
+            <i class="fas fa-envelope"></i> Kontak</a>
+        </li>
+                <li class="nav-item ms-2">
+          <a class="btn btn-outline-primary" href="admin/login.php">
+            <i class="fas fa-sign-in-alt"></i> Login</a>
+      </ul>
+    </div>
+  </div>
+</nav>
